be01898cd3a7829c02c15abbf55f16eae7a729de
dan <benzema-benarfa@bk.ru>
2022-10-04 11:05:01 +0500

    dist changed

--- src/router/index.js
+++ src/router/index.js
@@ -279,7 +279,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHashHistory('ignored'),
   routes,
   scrollBehavior() {
     // always scroll to top
